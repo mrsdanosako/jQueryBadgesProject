b036522ce307adb840af260f725f0469713d3867
Lorraine Osako <cl41@hawaii.edu>
2017-02-21 15:56:12 -1000

    2

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,9 +1,7 @@
-$.ajax({
-  $.ajax({
+.ajax({
     url: 'https://www.codeschool.com/users/mrsdanosako.json',
     dataType: 'jsonp',
     success: function(response) {
-      // handle response
     }
   });
-  // your code will go here
+ 
