ea740ddf8593b9ae1ecf6853e555152e5b215c79
Lorraine Osako <cl41@hawaii.edu>
2017-02-21 16:58:19 -1000

    3

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,7 +4,31 @@ $.ajax({
     url: 'https://www.codeschool.com/users/mrsdanosako.json',
     dataType: 'jsonp',
     success: function(response) {
-      console.log('response', response);
+      addCourses(response.courses.completed);
+
     }
   });
-});
+
+function addCourses(courses) {
+  var $badges = $('#badges');
+
+  courses.forEach(function(course) {
+    var $course = $('<div />', {
+      'class': 'course'
+    }).appendTo($badges);
+    $('<h3 />', {
+      text: course.title
+    }).appendTo($course);
+
+    $('<img />', {
+      src: course.badge
+    }).appendTo($course);
+    $('<a />', {
+      'class': 'btn btn-primary',
+      target: '_blank',
+      href: course.url,
+      text: 'See Course'
+    }).appendTo($course);
+   })
+  }
+ });
